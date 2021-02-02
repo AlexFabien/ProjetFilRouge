1715018aa7f50217f0977f4ee6245ab3466b7bef
ikigeni <ikigeni.ms@gmail.com>
2021-02-02 12:15:47 +0100

    ajout de l'api de la table repondu

---
+++ QuizApi/Dtos/Repondu/AllReponduDto.cs
@@ -0,0 +1,28 @@
+﻿using QuizApi.quiz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizApi.Dtos.Repondu
+{
+    public class AllReponduDto
+    {
+        public AllReponduDto()
+        {
+            ActeurHasQuestion = new HashSet<ActeurHasQuestion>();
+        }
+
+        public AllReponduDto(string libelle, int idEtatReponse)
+        {
+            Libelle = libelle;
+            IdEtatReponse = idEtatReponse;
+        }
+
+        public int IdEtatReponse { get; set; }
+        public string Libelle { get; set; }
+
+        public virtual ICollection<ActeurHasQuestion> ActeurHasQuestion { get; set; }
+    }
+    
+}
---
+++ QuizApi/Services/ReponduService.cs
@@ -0,0 +1,71 @@
+﻿using QuizApi.Dtos.Repondu;
+using QuizApi.quiz;
+using QuizApi.Repositories;
+using QuizApi.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizApi.Services
+{
+    public class ReponduService
+    {
+        private ReponduRepository reponduRepository;
+
+        public ReponduService()
+        {
+            this.reponduRepository = new ReponduRepository(new QueryBuilder());
+        }
+
+        internal List<AllReponduDto> FindAll()
+        {
+            List<Repondu> reponduEntities = reponduRepository.FindAll();
+            List<AllReponduDto> allRepondueDtos = new List<AllReponduDto>();
+            reponduEntities.ForEach(reponduEntity => { allRepondueDtos.Add(ConvertEntityToDto(reponduEntity)); });
+            return allRepondueDtos;
+        }
+
+        internal AllReponduDto Find(int id)
+        {
+            Repondu reponduEntity = this.reponduRepository.Find(id);
+            AllReponduDto allRepondueDtos = ConvertEntityToDto(reponduEntity);
+            return allRepondueDtos;
+        }
+
+        private AllReponduDto ConvertEntityToDto(Repondu reponduEntity)
+        {
+            return new AllReponduDto(reponduEntity.Libelle, reponduEntity.IdEtatReponse);
+        }
+
+        internal AfterCreateReponduDto PostReponduEntity(CreateReponduDto reponduEntity)
+        {
+            Repondu newtRepondu = transformDtoToEntity(reponduEntity);
+            Repondu newtReponduCreated = this.reponduRepository.Create(newtRepondu);
+            return transformEntityToAfterCreateDto(newtReponduCreated, true);
+        }
+
+        internal long Delete(int id)
+        {
+            return this.reponduRepository.Delete(id);
+        }
+
+        internal AfterCreateReponduDto PutReponduEntity(int id, CreateReponduDto newRepondu)
+        {
+            Repondu newtRepondu = transformDtoToEntity(newRepondu);
+            Repondu newtReponduUpdated = this.reponduRepository.Update(id, newtRepondu);
+            return transformEntityToAfterCreateDto(newtReponduUpdated, true);
+        }
+
+        private AfterCreateReponduDto transformEntityToAfterCreateDto(Repondu reponduEntity, bool isCreated)
+        {
+            return new AfterCreateReponduDto(reponduEntity.Libelle, isCreated, reponduEntity.IdEtatReponse);
+        }
+
+        private Repondu transformDtoToEntity(CreateReponduDto reponduEntity)
+        {
+            return new Repondu(reponduEntity.Libelle);
+        }
+    }
+}
+
