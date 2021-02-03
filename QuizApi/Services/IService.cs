249110ee56c331abbbb1afef0c46699fecc0eca3
ALEX Fabien <alexandre.fabien@laposte.net>
2021-02-03 10:15:14 +0100

    Ajout de IService

---
+++ QuizApi/Services/IService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizApi.Services
+{
+    interface IService<T>
+    {
+        /// <summary>
+        /// Méthode qui retourne la liste de toutes les T(DTO)
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> TrouverTout();
+
+        /// <summary>
+        /// Méthode qui retourne la T(DTO) qui a pour id : {id}
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public T TrouverParId(int id);
+
+        /// <summary>
+        /// Méthode qui ajoute une T à la base de données
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public void Ajouter(T obj);
+
+        /// <summary>
+        /// Méthode qui supprime la T qui a pour id : {id} de la base de données
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public void Supprimer(int id);
+
+        /// <summary>
+        /// Méthode qui modifie une obj
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public void Modifier(T obj);
+    }
+
+}
