74d6dc79996eae35d108759a02995ba058f45de2
ALEX Fabien <alexandre.fabien@laposte.net>
2021-03-11 14:40:50 +0100

    Ajout TypeQuestion (Repo, service, controller)

---
+++ QuizApi/Controllers/TypeQuestionsController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuizApi.Dtos;
+using QuizApi.Services;
+using QuizApi.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuizApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypesQuestionController : ControllerBase
+    {
+        private IService<TypeQuestionDto> service;
+
+        public TypesQuestionController(IService<TypeQuestionDto> service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult FindAll()
+        {
+            try
+            {
+                return Ok(this.service.TrouverTout());
+            }
+            catch (RessourceException e)
+            {
+                if (e.Statut == 404)
+                    return NotFound(e.Message);
+                else
+                {
+                    return BadRequest(e.Message);
+                }
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult FindById(int id)
+        {
+            try
+            {
+                return Ok(this.service.TrouverParId(id));
+            }
+            catch (RessourceException e)
+            {
+                if (e.Statut == 404)
+                    return NotFound(e.Message);
+                else
+                {
+                    return BadRequest(e.Message);
+                }
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                this.service.Supprimer(id);
+                return Ok();
+            }
+            catch (RessourceException e)
+            {
+                if (e.Statut == 404)
+                    return NotFound(e.Message);
+                else
+                {
+                    return BadRequest(e.Message);
+                }
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult Insert([FromBody] TypeQuestionDto typeQuestionDto)
+        {
+            try
+            {
+                typeQuestionDto = this.service.Ajouter(typeQuestionDto);
+#warning Retourner un code 201
+                return Ok(typeQuestionDto);
+            }
+            catch (RessourceException e)
+            {
+                if (e.Statut == 404)
+                    return NotFound(e.Message);
+                else
+                {
+                    return BadRequest(e.Message);
+                }
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpPut]
+        [Route("")]
+        public IActionResult Update([FromBody] TypeQuestionDto typeQuestionDto)
+        {
+            try
+            {
+                this.service.Modifier(typeQuestionDto);
+                return Ok(typeQuestionDto);
+            }
+            catch (RessourceException e)
+            {
+                if (e.Statut == 404)
+                    return NotFound(e.Message);
+                else
+                {
+                    return BadRequest(e.Message);
+                }
+            }
+            catch (Exception)
+            {
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+
+}
